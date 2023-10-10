fixed unnecessary computation graph generation which caused a memory leak in validation loop. also fixed a bug with checkpoint implementation.

decoder-transformer/model.py
@@ -1,10 +1,11 @@
-import logging
+from relative_logger import get_logger
 import torch
 from torch import tensor, sin, cos
 from math import sqrt
 from torch.nn.functional import softmax
 import torch.nn as nn
 
+logger = get_logger(__name__)
 
 def par_attention(queries: tensor, keys: tensor, values: tensor, dim: int) -> tensor:
     raw_weights = torch.bmm(queries, keys.transpose(1, 2))
@@ -70,10 +71,11 @@ class AttentionHead(nn.Module):
         return output
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, model_dim, num_heads):
+    def __init__(self, model_dim, num_heads, device):
         super().__init__()
         self.att_heads = nn.ModuleList([AttentionHead(model_dim, model_dim // num_heads) for _ in range(num_heads)])
         self.proj = nn.Linear(model_dim, model_dim, bias=False)
+        self.device = device
 
     def forward(self, x):
         head_outputs = [head(x) for head in self.att_heads]
@@ -82,9 +84,9 @@ class MultiHeadAttention(nn.Module):
         return x
         
 class TransformerLayer(nn.Module):
-    def __init__(self, model_dim, num_heads, ff_hidden_dim, context_len):
+    def __init__(self, model_dim, num_heads, ff_hidden_dim, context_len, device):
         super().__init__()
-        self.attention_block = MultiHeadAttention(model_dim, num_heads)
+        self.attention_block = MultiHeadAttention(model_dim, num_heads, device)
         self.norm1 = nn.LayerNorm(normalized_shape=[context_len, model_dim])
         self.ff1 = nn.Linear(model_dim, ff_hidden_dim)
         self.ff_relu = nn.ReLU()
@@ -108,13 +110,13 @@ class TransformerLayer(nn.Module):
 
 class TransformerNetwork(nn.Module):
     def __init__(self, output_dict_size: int, device: torch.device=None, context_len: int=16, num_layers=3, model_dim=256, att_heads=4, ff_hidden_dim=1024, name="model"):
-        logging.debug("Initializing model...")
+        logger.debug("Initializing model...")
         super().__init__()
         self.name = name
         self.device = device if device else torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         self.encode_embed = PositionalEncoding(model_dim, context_len, self.device)
-        self.trans_layers = nn.ModuleList([TransformerLayer(model_dim, att_heads, ff_hidden_dim, context_len) for _ in range(num_layers)])
+        self.trans_layers = nn.ModuleList([TransformerLayer(model_dim, att_heads, ff_hidden_dim, context_len, device) for _ in range(num_layers)])
         self.word_predictor = nn.Linear(model_dim * context_len, output_dict_size)
 
         self.context_len = context_len

decoder-transformer/train.py
@@ -1,5 +1,6 @@
-from relative_logger import get_logger
+import os
 import torch
+from relative_logger import get_logger
 from torch.utils.data import DataLoader
 from model import TransformerNetwork
 
@@ -54,22 +55,23 @@ class Trainer:
         logger.info(f"Average loss for training batches in this epoch: {avg_loss}")
 
         if do_validation:
-            self.model.train(False)
-            batches = 0
-            avg_loss = 0
-            for step, (features, labels) in enumerate(self.test_loader):
-                features, labels = features.to(self.device), labels.to(self.device)
-                preds = self.model(features)
-                loss = self.loss_func(preds, labels)
-
-                avg_loss += loss
-                batches = step + 1
-
-                del features
-                del labels
-
-            avg_loss = avg_loss / batches
-            logger.info(f"Average loss for validation batches in this epoch: {avg_loss}")
+            with torch.no_grad():
+                self.model.train(False)
+                batches = 0
+                avg_loss = 0
+                for step, (features, labels) in enumerate(self.test_loader):
+                    features, labels = features.to(self.device), labels.to(self.device)
+                    preds = self.model(features)
+                    loss = self.loss_func(preds, labels)
+    
+                    avg_loss += loss
+                    batches = step + 1
+    
+                    del features
+                    del labels
+    
+                avg_loss = avg_loss / batches
+                logger.info(f"Average loss for validation batches in this epoch: {avg_loss}")
 
     def train(self, epochs, do_val=True):
         for i in range(epochs):
@@ -84,4 +86,7 @@ class Trainer:
             "model_state_dict": self.model.state_dict(),
             "optimizer_state_dict": self.optimizer.state_dict(),
         }
-        torch.save(checkpoint, f"saved_models/{self.model.name}/checkpoint-epoch{epoch}.pth")
+        dir_path = f"saved-models/{self.model.name}"
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        torch.save(checkpoint, f"{dir_path}/checkpoint-epoch{epoch}.pth")